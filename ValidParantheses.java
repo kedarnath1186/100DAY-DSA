36ed40c39d9f47d7f703c4044d16cc1c9f2d78e6
kedarnath1186 <161944571+kedarnath1186@users.noreply.github.com>
2025-10-11 23:18:19 +0530

    Add files via upload

---
+++ ValidParantheses.java
@@ -0,0 +1,29 @@
+class Solution {
+public static void main(String []args){
+
+    String s="{}";
+    System.out.println(isValid(s));
+}
+
+    public static boolean isValid(String s) {
+        
+        while(true)
+        {
+            if(s.contains("()"))
+            {
+                s=s.replace("()","");            
+            }
+            else if(s.contains("[]"))
+            {
+                s=s.replace("[]","");            
+            }else if(s.contains("{}"))
+            {
+                s=s.replace("{}","");            
+            }
+            else{
+                return s.isEmpty();
+
+            }
+        }
+    }
+}
